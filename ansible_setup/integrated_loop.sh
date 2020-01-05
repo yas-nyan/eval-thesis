35aa301991a8a39c2f3979532b8537d57be00c1b
yas-nyan <yas-nyan@sfc.wide.ad.jp>
2020-01-05 18:17:37 +0900

    順番逆

--- ansible_setup/integrated_loop.sh
+++ ansible_setup/integrated_loop.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ansible-playbook -i hosts pods.yml
-
+sleep 300 # コンバージェンス時間待機
 ansible-playbook -i hosts add_server.yml
 
-for i in `seq 0 20`
-do
-    ansible-playbook -i hosts experiment_add_server.yml
-done
-
-for i in `seq 0 20`
+for i in `seq 0 30`
 do
     ansible-playbook -i hosts experiment_remove_server.yml
+done
+
+for i in `seq 0 30`
+do
+    ansible-playbook -i hosts experiment_add_server.yml
 done
